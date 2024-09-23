4fe239ca07dad3e39926f4f6f82e01e52a31bf02
geeg <136278046+geoscreuza@users.noreply.github.com>
2024-09-23 11:47:46 -0300

    3q6h

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ function soma(num1,num2){
     return num1+num2;
 }
 console.log(soma (1,2));
+
 function areaQuadrado(lado){
     return lado**2
 }
 console.log(areaQuadrado(6))
+
+let areaTriangulo=function(base,altura){
+    return base*altura/2
+}
+console.log(areaTriangulo(10,20))
