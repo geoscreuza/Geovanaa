a37ee3be58fe3e4e5a2ce8e84f4f2682e7fadaa2
geeg <136278046+geoscreuza@users.noreply.github.com>
2024-09-30 12:15:56 -0300

    4qn

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ for (let i=0; i<estados.length; i++){
     console.log(estados[i]);
 }
 
-let idade = 18
+let idade = parseInt (prompt('qual a sua idade?'))
 if (idade>17) {
     console.log('já pode casar');
 }
