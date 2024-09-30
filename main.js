a6395ba1a2770aeb39ec1307849eb42014db6527
geeg <136278046+geoscreuza@users.noreply.github.com>
2024-09-30 11:39:59 -0300

    76

--- main.js
+++ main.js
@@ -23,4 +23,7 @@
 // }
 // document.write(media(mediaAritmetica1,mediaAritmetica2,mediaAritmetica3));
 
-let estados = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins','Brasília(DF)',]
+let estados = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins','Brasília(DF)',];
+let i = 0;
+while(i<10){}
+console.log(i);
