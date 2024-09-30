3ccabe76127a6bcdc9e3aacb4171445da15dfd18
geeg <136278046+geoscreuza@users.noreply.github.com>
2024-09-30 11:23:16 -0300

    yt

--- main.js
+++ main.js
@@ -24,4 +24,3 @@
 // document.write(media(mediaAritmetica1,mediaAritmetica2,mediaAritmetica3));
 
 let estados = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins','Brasília(DF)',]
- estados
