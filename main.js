b98bf7b74357c3466340737deab9f8f09c83b934
geeg <136278046+geoscreuza@users.noreply.github.com>
2024-09-30 11:20:13 -0300

    gj

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
- let nomeUsuario = 'geovana';
- alert (nomeUsuario);
-function soma(num1,num2){
-    return num1+num2;
-}
-console.log(soma (1,2));
+//  let nomeUsuario = 'geovana';
+//  alert (nomeUsuario);
+// function soma(num1,num2){
+//     return num1+num2;
+// }
+// console.log(soma (1,2));
 
-function areaQuadrado(lado){
-    return lado**2
-}
-console.log(areaQuadrado(6))
+// function areaQuadrado(lado){
+//     return lado**2
+// }
+// console.log(areaQuadrado(6))
 
-let areaTriangulo=function(base,altura){
-    return (base*altura)/2
-}
-console.log(areaTriangulo(10,20))
+// let areaTriangulo=function(base,altura){
+//     return (base*altura)/2
+// }
+// console.log(areaTriangulo(10,20))
 
-let mediaAritmetica1=parseInt(prompt('numero 1'));
-let mediaAritmetica2=parseInt(prompt('numero 2'));
-let mediaAritmetica3=parseInt(prompt('numero 3'));
-function media(num1,num2,num3){
-    return(num1+num2+num3)/3;
-}
-document.write(media(mediaAritmetica1,mediaAritmetica2,mediaAritmetica3));
+// let mediaAritmetica1=parseInt(prompt('numero 1'));
+// let mediaAritmetica2=parseInt(prompt('numero 2'));
+// let mediaAritmetica3=parseInt(prompt('numero 3'));
+// function media(num1,num2,num3){
+//     return(num1+num2+num3)/3;
+// }
+// document.write(media(mediaAritmetica1,mediaAritmetica2,mediaAritmetica3));
+
+let estados = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins','Brasília(DF)',]
